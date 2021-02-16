b3ae1e86b1586e731726ffd2c87222c6e8e7d763
EC2 Default User <ec2-user@ip-172-31-37-102.ec2.internal>
2021-02-16 05:28:42 +0000

    Fixing

--- app.py
+++ app.py
@@ -118,7 +118,9 @@ def hello_world():
     )
     
 app.run(
-    port = int(os.getenv('PORT', process.env.PORT)),
-    debug = True
+    host=os.getenv('IP', '0.0.0.0'),
+    port=int(os.getenv('PORT', 8080)),
+    debug=True,
+    use_reloader=True
     )
     
